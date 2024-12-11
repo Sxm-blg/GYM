b8ee44543fa102e2aec7372cfaf946a3de5479e2
Sxm-Blg <simon.belogi@gmail.com>
2024-12-11 12:59:14 +0100

    added package
    added Projekt

---
+++ gymTracker/src/user/User.java
@@ -0,0 +1,13 @@
+package user;
+
+public class User {
+	private String name;
+	private double height;
+	private double weight;
+	
+	private String eMail;
+	private String password;
+	
+	private int membershipNumber;
+	private int a;
+}
